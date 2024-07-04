5e0b61dd373ea3db10e4a5deb59119c88dbfbd78
DI-GIUSTO-Alex1987 <digiusto.alex@tutanota.com>
2024-07-04 11:31:42 +0200

    Version 1.1 Installation de l environnement de travail

---
+++ wp-content/plugins/rocket-lazy-load/vendor/composer/installed.php
@@ -0,0 +1,32 @@
+<?php return array(
+    'root' => array(
+        'name' => 'wp-media/rocket-lazy-load',
+        'pretty_version' => 'v2.3.8',
+        'version' => '2.3.8.0',
+        'reference' => 'f292de268086bb4d97293eac5b3262693d34f6d7',
+        'type' => 'wordpress-plugin',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => false,
+    ),
+    'versions' => array(
+        'wp-media/rocket-lazy-load' => array(
+            'pretty_version' => 'v2.3.8',
+            'version' => '2.3.8.0',
+            'reference' => 'f292de268086bb4d97293eac5b3262693d34f6d7',
+            'type' => 'wordpress-plugin',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+        'wp-media/rocket-lazyload-common' => array(
+            'pretty_version' => 'v2.5.12',
+            'version' => '2.5.12.0',
+            'reference' => '09bdc9a550b8bd1197c155a98e17c45adcb190af',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../wp-media/rocket-lazyload-common',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
